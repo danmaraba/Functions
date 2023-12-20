147767914e0842a4f0a1b2683a8b9e19e3c066d4
danmaraba <danielkmaraba@gmail.com>
2023-12-20 11:29:37 +0300

    Declare a variable and assign anonymous function

---
+++ index.js
@@ -0,0 +1,6 @@
+// Anonymous functions-simply functions without a name
+const fn=function (){
+    console.log("You will go to the office");
+}
+fn();
+// One way of accessing anonymous function is by declaring a variable and assigning the function as its value
